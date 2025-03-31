d3e9c9deb7f397d303c0ae73f13547854fa6f7c5
Evgenija <evgenijagrujevska@gmail.com>
2025-03-31 16:14:14 +0200

    Implement function List tasks sorted by name

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -76,6 +76,12 @@ class TaskManager {
     // 3. List tasks sorted by name
     public void sortTasksByName() {
         // TODO: Implement sorting logic
+        Comparator<Task> nameComparator = new Comparator<Task>() {
+        public int compare(Task t1, Task t2) {
+            return t1.getName().compareTo(t2.getName());
+            }
+        };
+        Collections.sort(tasks, nameComparator);
     }
 
     // 4. Sort tasks by priority
